f3fd6693ca8e6a4dfcf3306f0fc22bc2251eed14
jabber <2898117012@qq.com>
2022-07-04 01:59:05 +0800

    init

--- shopeo-services.php
+++ shopeo-services.php
@@ -13,6 +13,23 @@
  * Requires PHP: 5.6
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Define SHOPEO_SERVICES_PLUGIN_FILE.
+if ( ! defined( 'SHOPEO_SERVICES_PLUGIN_FILE' ) ) {
+	define( 'SHOPEO_SERVICES_PLUGIN_FILE', __FILE__ );
+}
+
+if ( ! defined( 'SHOPEO_SERVICES_PLUGIN_BASE' ) ) {
+	define( 'SHOPEO_SERVICES_PLUGIN_BASE', plugin_basename( SHOPEO_SERVICES_PLUGIN_FILE ) );
+}
+
+if ( ! defined( 'SHOPEO_SERVICES_PATH' ) ) {
+	define( 'SHOPEO_SERVICES_PATH', plugin_dir_path( SHOPEO_SERVICES_PLUGIN_FILE ) );
+}
+
 if ( ! function_exists( 'shopeo_services_activate' ) ) {
 	function shopeo_services_activate() {
 
@@ -28,3 +45,11 @@ if ( ! function_exists( 'shopeo_services_deactivate' ) ) {
 }
 
 register_deactivation_hook( __FILE__, 'shopeo_services_deactivate' );
+
+if ( ! function_exists( 'shopeo_services_load_textdomain' ) ) {
+	function shopeo_services_load_textdomain() {
+		load_plugin_textdomain( 'shopeo-services', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+}
+
+add_action( 'init', 'shopeo_services_load_textdomain' );
