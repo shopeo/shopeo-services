4eee9b626fb5894a0a3a2cde9390000f68efbf5f
jabber <2898117012@qq.com>
2022-07-07 07:41:48 +0800

    init

--- shopeo-services.php
+++ shopeo-services.php
@@ -40,7 +40,7 @@ register_activation_hook( __FILE__, 'shopeo_services_activate' );
 
 if ( ! function_exists( 'shopeo_services_deactivate' ) ) {
 	function shopeo_services_deactivate() {
-
+		unregister_post_type( 'shopeo_service' );
 	}
 }
 
@@ -53,3 +53,52 @@ if ( ! function_exists( 'shopeo_services_load_textdomain' ) ) {
 }
 
 add_action( 'init', 'shopeo_services_load_textdomain' );
+
+if ( ! function_exists( 'shopeo_services_post_type' ) ) {
+	function shopeo_services_post_type() {
+		register_post_type( 'shopeo_service', array(
+			'label'              => __( 'Services', 'shopeo-services' ),
+			'labels'             => array(
+				'name'               => __( 'Services', 'shopeo-services' ),
+				'all_items'          => __( 'All Services', 'shopeo-services' ),
+				'singular_name'      => __( 'Service', 'shopeo-services' ),
+				'add_new_item'       => __( 'Add New Service', 'shopeo-services' ),
+				'edit_item'          => __( 'Edit Service', 'shopeo-services' ),
+				'view_item'          => __( 'View Service', 'shopeo-services' ),
+				'view_items'         => __( 'View Services', 'shopeo-services' ),
+				'search_items'       => __( 'Search Services', 'shopeo-services' ),
+				'not_found'          => __( 'No Services found.', 'shopeo-services' ),
+				'not_found_in_trash' => __( 'No Services found in trash.', 'shopeo-services' ),
+				'archives'           => __( 'Service Archives', 'shopeo-services' ),
+				'attributes'         => __( 'Service Attributes', 'shopeo-services' ),
+			),
+			'public'             => true,
+			'hierarchical'       => true,
+			'publicly_queryable' => true,
+			'has_archive'        => true,
+			'menu_position'      => 5,
+			'menu_icon'          => 'dashicons-heart',
+			'show_in_rest'       => true,
+			'rest_base'          => 'services',
+			'map_meta_cap'       => true,
+			'supports'           => array(
+				'title',
+				'editor',
+				'comments',
+				'revisions',
+				'trackbacks',
+				'excerpt',
+				'page-attributes',
+				'thumbnail',
+				'custom-fields',
+				'post-formats'
+			),
+			'rewrite'            => array(
+				'slug' => 'services'
+			),
+			'query_var'          => false,
+		) );
+	}
+}
+
+add_action( 'init', 'shopeo_services_post_type' );
